4c832805427db2fedc9b294e6f17da55c845fccf
JakeTheReaper <jake.zerafa86@gmail.com>
2018-01-02 23:01:15 +1100

    Updated Web Code

--- wp/a2/login.php
+++ wp/a2/login.php
@@ -21,9 +21,13 @@
 	</nav>
 
 	<main>
-		<?php
-		echo "Website Under Construction"
-		?>			
+		<div class="login">
+            <form action="https://titan.csit.rmit.edu.au/~e54061/wp/processing.php" method="post">
+                Email Address:<input type="email" name="email" placeholder="example@example.com"><br>
+                Password:<input type="password" name="password" placeholder="********"><br><br>
+                <input type="submit" value="Submit">
+            </form>
+        </div>			
 	</main>
 		
 	<footer>
