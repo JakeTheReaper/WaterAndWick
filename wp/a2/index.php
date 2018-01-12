a235c7f1dd90fbc8886840de74b028ec71000c7f
Jake Zerafa <jake.zerafa86@gmail.com>
2018-01-13 00:51:54 +1100

    Latest Code Update

--- wp/a2/index.php
+++ wp/a2/index.php
@@ -1,50 +1,52 @@
 <!DOCTYPE html>
 <html>
-    <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.css" />
-	<link rel="stylesheet" type="text/css" href="style.css"/>
-	<title>Water and Wick</title>
-    </head>	
 
-    <body>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.css" />
+    <link rel="stylesheet" type="text/css" href="style.css" />
+    <title>Water and Wick</title>
+</head>
+
+<body>
     <section class="main-logo">
-	    <div class="container">
-	        <div class="row">
-                <div class="one">
-                    <img src="../img/WebsiteLogo.png" alt="Water & Wick Logo" class="main-logo-img">  
+        <div class="container">
+            <div class="row">
+                <div>
+                    <h1 class="main-heading">Weclome To Water & Wick</h1>
                 </div>
-	        </div>
-	    </div>
-	</section>
-	<section class="section section-one">
-	    <div class="container">
-	        <div class="row">
-	            <div class="one-third column">
+            </div>
+        </div>
+    </section>
+    <section class="section section-one">
+        <div class="container">
+            <div class="row">
+                <div class="one-third column">
                     <a href="index.php">
-                    <h5>Home</h5>
-	                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus placerat dictum urna at iaculis. Proin vehicula tellus eget efficitur faucibus. Vivamus et nunc nulla.</p>
+                        <h5>Home</h5>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus placerat dictum urna at iaculis. Proin vehicula tellus eget efficitur faucibus. Vivamus et nunc nulla.</p>
                     </a>
-	            </div>
-	            <div class="one-third column">
-	                <h5>Products</h5>
-	                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus placerat dictum urna at iaculis. Proin vehicula tellus eget efficitur faucibus. Vivamus et nunc nulla.</p>
-	            </div>
-	            
-	            <div class="one-third column">
+                </div>
+                <div class="one-third column">
+                    <h5>Products</h5>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus placerat dictum urna at iaculis. Proin vehicula tellus eget efficitur faucibus. Vivamus et nunc nulla.</p>
+                </div>
+
+                <div class="one-third column">
                     <a href="login.php">
-	                <h5>Login</h5>
-	                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus placerat dictum urna at iaculis. Proin vehicula tellus eget efficitur faucibus. Vivamus et nunc nulla.</p>
-	                </a>
-	            </div>
-	            
-	        </div>
-	    </div>
-	</section>
-		
-	<footer>
-	</footer>
-    </body>
+                        <h5>Login</h5>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus placerat dictum urna at iaculis. Proin vehicula tellus eget efficitur faucibus. Vivamus et nunc nulla.</p>
+                    </a>
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+    <footer>
+    </footer>
+</body>
+
 </html>
--- wp/a2/login.php
+++ wp/a2/login.php
@@ -1,54 +1,57 @@
 <!DOCTYPE html>
 <html>
-    <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.css" />
-	<link rel="stylesheet" type="text/css" href="style.css"/>
-	<title>Water and Wick</title>
-    </head>
 
-    <body>
-	<main>
-		<div class="container">
-	        <div class="row">
-           <div class="u-full-width">
-            <form action="https://titan.csit.rmit.edu.au/~e54061/wp/processing.php" method="post">
-                Email Address:<input type="email" name="email" placeholder="example@example.com"><br>
-                Password:<input type="password" name="password" placeholder="********"><br><br>
-                <input class="button-primary" type="submit" value="Submit">
-            </form>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.css" />
+    <link rel="stylesheet" type="text/css" href="style.css" />
+    <title>Water and Wick</title>
+</head>
+
+<body>
+    <main>
+        <div class="container">
+            <div class="row">
+                <div class="six columns">
+                    <form action="https://titan.csit.rmit.edu.au/~e54061/wp/processing.php" method="post">
+                        Email Address:
+                        <input class="u-full-width" type="email" name="email" placeholder="example@example.com">Password:
+                        <input class="u-full-width" type="password" name="password" placeholder="********"><br><br>
+                        <input class="button-primary" type="submit" value="Submit">
+                    </form>
+                </div>
             </div>
-        </div>	
-        </div>		
-	</main>
-	<section class="section section-one">
-	    <div class="container">
-	        <div class="row">
-	            <div class="one-third column">
-	                <a href="index.php">
-	                <h5>Home</h5>
-	                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus placerat dictum urna at iaculis. Proin vehicula tellus eget efficitur faucibus. Vivamus et nunc nulla.</p>
+        </div>
+    </main>
+    <section class="section section-one">
+        <div class="container">
+            <div class="row">
+                <div class="one-third column">
+                    <a href="index.php">
+                        <h5>Home</h5>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus placerat dictum urna at iaculis. Proin vehicula tellus eget efficitur faucibus. Vivamus et nunc nulla.</p>
                     </a>
-	            </div>
-	            <div class="one-third column">
-	                <h5>Products</h5>
-	                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus placerat dictum urna at iaculis. Proin vehicula tellus eget efficitur faucibus. Vivamus et nunc nulla.</p>
-	            </div>
-	            
-	            <div class="one-third column">
+                </div>
+                <div class="one-third column">
+                    <h5>Products</h5>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus placerat dictum urna at iaculis. Proin vehicula tellus eget efficitur faucibus. Vivamus et nunc nulla.</p>
+                </div>
+
+                <div class="one-third column">
                     <a href="login.php">
-	                <h5>Login</h5>
-	                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus placerat dictum urna at iaculis. Proin vehicula tellus eget efficitur faucibus. Vivamus et nunc nulla.</p>
-	                </a>
-	            </div>
-	            
-	        </div>
-	    </div>
-	</section>
-		
-	<footer>
-	</footer>
-    </body>
+                        <h5>Login</h5>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus placerat dictum urna at iaculis. Proin vehicula tellus eget efficitur faucibus. Vivamus et nunc nulla.</p>
+                    </a>
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+    <footer>
+    </footer>
+</body>
+
 </html>
